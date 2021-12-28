fafd675eda7ac8964929eae501761b67e2fb628c
devgho <devgho@outlook.com>
2021-12-28 19:47:06 +0800

    first commit

---
+++ dlwangzhe.py
@@ -0,0 +1,18 @@
+import requests
+import re
+import os
+
+url = "https://pvp.qq.com/zlkdatasys/mct/d/play.shtml"
+res = requests.get(url)
+res.encoding = "gbk"
+all = res.text
+# print(res.text)
+url = re.search("https://dlied4.myapp.com/myapp/.*?apk",all).group()
+filename = re.search(r"(?P<u>.*/)(?P<filename>.*?apk)",url).group("filename")
+print(filename)
+if os.path.exists(filename):
+    pass
+else:
+    with open("/home/data/jymdata/pubg/"+filename,"wb") as f:
+        f.write(requests.get(url).content)
+res.close()
---
+++ wangzhe.sh
@@ -0,0 +1,9 @@
+filename=`python /home/data/jymdata/auto_spider_apk/pubg/dlwangzhe.py`
+name=`/home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/$filename|grep package|grep -oP name=\'.*?\'|grep -o \'.*\'`
+version_code=`/home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/$filename|grep package|grep -oP versionCode=\'.*?\'|grep -o \'.*\'`
+version_name=`/home/fujianfeng/android-9/aapt dump badging /home/data/jymdata/auto_spider_apk/pubg/$filename|grep package|grep -oP VersionName=\'.*?\'|grep -o \'.*\'`
+name=`echo $name|sed "s/^\([\"']\)\(.*\)\1\$/\2/g"`
+version_code=`echo $version_code|sed "s/^\([\"']\)\(.*\)\1\$/\2/g"`
+version_name=`echo $version_name|sed "s/^\([\"']\)\(.*\)\1\$/\2/g"`
+echo "{\"version_code\":\"${version_code}\",\"version_name\":\"${version_name}\",\"last_filename\":\"${name}_${version_code}.apk\"}" > /home/data/jymdata/apks/$name.json
+cp /home/data/jymdata/auto_spider_apk/pubg/$filename /home/data/jymdata/apks/$name\_$version_code.apk
