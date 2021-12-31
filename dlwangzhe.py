2733f0bf8503bf1213e4f4ab5ef8829135aea121
devgho <devgho@outlook.com>
2021-12-31 15:49:21 +0800

    change url

--- dlwangzhe.py
+++ dlwangzhe.py
@@ -2,11 +2,10 @@ import requests
 import re
 import os
 
-url = "https://pvp.qq.com/zlkdatasys/mct/d/play.shtml"
+url = "https://696214c97ffe3b0032745108cb06ef60.dlied1.cdntips.net/godlied4.myapp.com/myapp/6337/cos.static-77964/1104466820.js?mkey=61ce95bcdef462cc&f=0ae6&time=1596093184&cip=222.244.68.57&proto=https&access_type="
 res = requests.get(url)
 res.encoding = "gbk"
 all = res.text
-# print(res.text)
 url = re.search("https://dlied4.myapp.com/myapp/.*?apk",all).group()
 filename = re.search(r"(?P<u>.*/)(?P<filename>.*?apk)",url).group("filename")
 print(filename)
